f2e13284a143662c6c86b5764cc483d7058796d7
MostafaWasfyElbaz <mostafawasfyelbaz@gmail.com>
2026-04-09 23:53:10 +0200

    'update'

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -36,7 +36,7 @@ export const bootstrap = async (): Promise<void> => {
       });
     },
   );
-  app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+  app.listen(process.env.SERVER_PORT || 5000, () => {
+    console.log(`Server is running on port ${process.env.SERVER_PORT || 5000}`);
   });
 };
