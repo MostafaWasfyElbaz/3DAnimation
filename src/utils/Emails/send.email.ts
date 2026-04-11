3d2d3b52e1247341a2f21311f01dde512086d001
MostafaWasfyElbaz <mostafawasfyelbaz@gmail.com>
2026-04-11 09:29:49 +0200

    'final'

--- src/utils/Emails/send.email.ts
+++ src/utils/Emails/send.email.ts
@@ -1,29 +1,44 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 
 export const sendEmail = async (
   email: string,
   subject: string,
   html: string,
 ) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST as string,
-    port: Number(process.env.EMAIL_PORT) as number,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER as string ,
-      pass: process.env.EMAIL_PASS as string,
-    },
-  });
+  try {
+    const OAuth2 = google.auth.OAuth2;
+    const oAuth2Client = new OAuth2(
+      process.env.OAUTH_CLIENT_ID as string,
+      process.env.OAUTH_CLIENT_SECRET as string,
+      "https://developers.google.com/oauthplayground",
+    );
 
-  const main = async () => {
+    oAuth2Client.setCredentials({
+      refresh_token: process.env.OAUTH_REFRESH_TOKEN as string,
+    });
+
+    const accessToken = (await oAuth2Client.getAccessToken()).token;
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL_USER as string,
+        clientId: process.env.OAUTH_CLIENT_ID as string,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN as string,
+        accessToken: accessToken as string,
+      },
+    } as any);
+
     await transporter.sendMail({
-      from: `Social App <${process.env.EMAIL_USER}>`,
+      from: `3DAnimation <${process.env.EMAIL_USER}>`,
       to: email,
       subject: subject,
       html: html,
     });
-  };
-  main().catch((err) => {
-    throw err;
-  });
+  } catch (error) {
+    throw error;
+  }
 };
